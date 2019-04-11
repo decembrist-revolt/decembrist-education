611ba528e7563f89bb7279076f98f007e894e265
nikita.pometun <decembrist.org@gmail.com>
2019-04-12 01:06:55 +0300

    java-ex1 info file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
+/**
+ * Задача: Вывести в консоль сообщение "Hello world!"
+ *
+ * Проверяется: System.out
+ */
 public class Main {
 
+    /**
+     * Метод main с сигнатурой public static void main(String[] args)
+     * является точкой входа в программу
+     * Java исполнит этот метод первым
+     */
     public static void main(String[] args) {
-
+        //Удалите эту строку и напишите на её месте свой код
     }
 
 }
